d3540ffac4b4dcf53ffa11f5ef580dc5baac7351
S2 <s@ireoo.com>
2018-09-12 21:05:15 +0800

    init

--- ROUTES/shadowsocks.js
+++ ROUTES/shadowsocks.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const axios = require("axios");
-const base64_encode = require('base64').encode;
 
 exports = module.exports = router;
 
@@ -12,7 +11,7 @@ router.all("/", (req, res, next) => {
         console.log(r.data);
         let ss = [];
         r.data.forEach(v => {
-            let name = base64_encode(`${v[6]} - ${v[5]}`);
+            let name = `${v[6]} - ${v[5]}`.toString('base64');
             let s = `${v[1]}:${v[2]}:origin:${v[3]}:plain:${v[4]}/?obfsparam=&remarks=${name}&group=aXNoYWRvd3NvY2tzLmhlcm9rdWFwcC5jb20vc2hhZG93c29ja3MK`;
             ss.push(s);
         });
