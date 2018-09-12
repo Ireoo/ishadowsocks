bb6ae194cb3768e3cadfbc6a19a88529ae141aae
S2 <s@ireoo.com>
2018-09-12 21:19:05 +0800

    init

--- ROUTES/shadowsocks.js
+++ ROUTES/shadowsocks.js
@@ -8,19 +8,15 @@ router.auth = true;
 
 router.all("/", (req, res, next) => {
     axios.get(`https://free-ss.site/ss.json?_=${new Date().getTime()}`).then(r => {
-        console.log(r.data);
+        // console.log(r.data);
         let ss = [];
-        if(r.data) {
-            r.data.forEach(v => {
-                let name = new Buffer(`${v[6]} - ${v[5]}`).toString('base64');
-                let s = new Buffer(`${v[1]}:${v[2]}:origin:${v[3]}:plain:${v[4]}/?obfsparam=&remarks=${name}&group=aXNoYWRvd3NvY2tzLmhlcm9rdWFwcC5jb20vc2hhZG93c29ja3MK`).toString("base64");
-                // console.log(s);
-                ss.push(s);
-            });
-            res.status(200).send(ss.join("\r\n"));
-        } else {
-            res.status(404).send(r);
-        }
+        r.data.data.forEach(v => {
+            let name = new Buffer(`${v[6]} - ${v[5]}`).toString('base64');
+            let s = new Buffer(`${v[1]}:${v[2]}:origin:${v[3]}:plain:${v[4]}/?obfsparam=&remarks=${name}&group=aXNoYWRvd3NvY2tzLmhlcm9rdWFwcC5jb20vc2hhZG93c29ja3MK`).toString("base64");
+            // console.log(s);
+            ss.push(s);
+        });
+        res.status(200).send(ss.join("\r\n"));
     }).catch(err => {
         res.status(404).send(err.message);
     });
