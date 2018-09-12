667a3859a11fef22e3da2e1079c1943efc82d2fa
S2 <s@ireoo.com>
2018-09-12 23:44:46 +0800

    init

--- ROUTES/shadowsocks.js
+++ ROUTES/shadowsocks.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const axios = require("axios");
+const base64js = require('base64-js');
 
 exports = module.exports = router;
 
@@ -11,18 +12,18 @@ router.all("/", (req, res, next) => {
         // console.log(r.data);
         let ss = [];
         r.data.data.forEach(v => {
-            let name = new Buffer(`${v[6]} - iShadowsocks`).toString("base64");
-            let group = new Buffer("iShadowsocks").toString("base64");
+            let name = base64js(`${v[6]} - iShadowsocks`);
+            let group = base64js("iShadowsocks");
             let ip = v[1],
                 port = v[2],
                 code = v[3],
                 pass = v[4];
             let address = `${ip}:${port}:origin:${code}:plain:${pass}/?remarks=${name}&group=${group}`;
-            let s = "ssr://" + new Buffer(address).toString("base64");
+            let s = "ssr://" + base64js(address);
             console.log(`ssr://${address}`);
             ss.push(s);
         });
-        res.status(200).send(new Buffer(ss.join("\r\n")).toString("base64"));
+        res.status(200).send(base64js(ss.join("\r\n")));
     }).catch(err => {
         res.status(404).send(err.message);
     });
