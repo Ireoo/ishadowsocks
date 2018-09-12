a42123ba06f4aa0ef179c0efdf7937013a839ee4
S2 <s@ireoo.com>
2018-09-12 21:09:06 +0800

    init

--- ROUTES/shadowsocks.js
+++ ROUTES/shadowsocks.js
@@ -8,12 +8,12 @@ router.auth = true;
 
 router.all("/", (req, res, next) => {
     axios.get(`https://free-ss.site/ss.json?_=${new Date().getTime()}`).then(r => {
-        console.log(r.data);
+        // console.log(r.data);
         let ss = [];
         r.data.forEach(v => {
-            let name = `${v[6]} - ${v[5]}`.toString('base64');
+            let name = new Buffer(`${v[6]} - ${v[5]}`).toString('base64');
             let s = `${v[1]}:${v[2]}:origin:${v[3]}:plain:${v[4]}/?obfsparam=&remarks=${name}&group=aXNoYWRvd3NvY2tzLmhlcm9rdWFwcC5jb20vc2hhZG93c29ja3MK`;
-            console.log(s);
+            // console.log(s);
             ss.push(s);
         });
         res.status(200).send(ss.join("\r\n"));
