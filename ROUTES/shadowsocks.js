d2903f33c82715424d476ce835c160da5155d7fb
S2 <s@ireoo.com>
2018-09-12 21:11:13 +0800

    init

--- ROUTES/shadowsocks.js
+++ ROUTES/shadowsocks.js
@@ -12,12 +12,12 @@ router.all("/", (req, res, next) => {
         let ss = [];
         r.data.forEach(v => {
             let name = new Buffer(`${v[6]} - ${v[5]}`).toString('base64');
-            let s = `${v[1]}:${v[2]}:origin:${v[3]}:plain:${v[4]}/?obfsparam=&remarks=${name}&group=aXNoYWRvd3NvY2tzLmhlcm9rdWFwcC5jb20vc2hhZG93c29ja3MK`;
+            let s = new Buffer(`${v[1]}:${v[2]}:origin:${v[3]}:plain:${v[4]}/?obfsparam=&remarks=${name}&group=aXNoYWRvd3NvY2tzLmhlcm9rdWFwcC5jb20vc2hhZG93c29ja3MK`).toString("base64");
             // console.log(s);
             ss.push(s);
         });
         res.status(200).send(ss.join("\r\n"));
     }).catch(err => {
-        res.status(404).send(err);
+        res.status(404).send(err.message);
     });
 });
