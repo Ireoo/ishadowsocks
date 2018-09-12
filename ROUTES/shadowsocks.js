775e8dbdb492c93bd0461e19370a86fa3f8321ab
S2 <s@ireoo.com>
2018-09-12 20:43:01 +0800

    init

--- ROUTES/shadowsocks.js
+++ ROUTES/shadowsocks.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const axios = require("axios");
+const base64_encode = require('base64').encode;
 
 exports = module.exports = router;
 
@@ -9,7 +10,13 @@ router.auth = true;
 router.all("/", (req, res, next) => {
     axios.get(`https://free-ss.site/ss.json?_=${new Date().getTime()}`).then(r => {
         console.log(r.data);
-        res.status(200).send(r.data);
+        let ss = [];
+        r.data.forEach(v => {
+            let name = base64_encode(`${v[6]} - ${v[5]}`);
+            let s = `${v[1]}:${v[2]}:origin:${v[3]}:plain:${v[4]}/?obfsparam=&remarks=${name}&group=aXNoYWRvd3NvY2tzLmhlcm9rdWFwcC5jb20vc2hhZG93c29ja3MK`;
+            ss.push(s);
+        });
+        res.status(200).send(ss.join("\r\n"));
     }).catch(err => {
         res.status(404).send(err);
     });
