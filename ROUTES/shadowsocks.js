4a35a57f30bb4af5ab8c93d40fec8bd621ff44df
S2 <s@ireoo.com>
2018-09-13 11:31:12 +0800

    init

--- ROUTES/shadowsocks.js
+++ ROUTES/shadowsocks.js
@@ -8,22 +8,22 @@ router.auth = true;
 // router.path = "/";
 
 router.all("/", (req, res, next) => {
-    axios.get(`https://free-ss.site/ss.json?_=${new Date().getTime()}`).then(r => {
+    axios.get(`https://raw.githubusercontent.con/ImLaoD/sub/master/ssrshare.com`).then(r => {
         // console.log(r.data);
-        let ss = [];
-        r.data.data.forEach(v => {
-            let name = Base64.encode(`${v[6]} - iShadowsocks`);
-            let group = Base64.encode("iShadowsocks");
-            let ip = v[1],
-                port = v[2],
-                code = v[3],
-                pass = v[4];
-            let address = `${ip}:${port}:origin:${code}:plain:${pass}/?remarks=${name}&group=${group}`;
-            let s = "ssr://" + Base64.encode(address).replace("=", "");
-            console.log(`ssr://${address}`);
-            ss.push(s);
-        });
-        res.status(200).send(Base64.encode(ss.join("\r\n")));
+        // let ss = [];
+        // r.data.data.forEach(v => {
+        //     let name = Base64.encode(`${v[6]} - iShadowsocks`);
+        //     let group = Base64.encode("iShadowsocks");
+        //     let ip = v[1],
+        //         port = v[2],
+        //         code = v[3],
+        //         pass = v[4];
+        //     let address = `${ip}:${port}:origin:${code}:plain:${pass}/?remarks=${name}&group=${group}`;
+        //     let s = "ssr://" + Base64.encode(address).replace("=", "");
+        //     console.log(`ssr://${address}`);
+        //     ss.push(s);
+        // });
+        res.status(200).send(r.data);
     }).catch(err => {
         res.status(404).send(err.message);
     });
