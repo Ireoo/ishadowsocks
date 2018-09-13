342c57df03fcde52de161cac9001d24d8e85dc40
S2 <s@ireoo.com>
2018-09-13 22:49:38 +0800

    init

--- ROUTES/shadowsocks.js
+++ ROUTES/shadowsocks.js
@@ -1,7 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+
 const router = require("express").Router();
 const axios = require("axios");
 const {Base64} = require('js-base64');
-const freeShadowsocks = require("free-shadowsocks");
+const gitPullOrClone = require("git-pull-or-clone");
 
 exports = module.exports = router;
 
@@ -9,20 +12,33 @@ router.auth = true;
 // router.path = "/";
 
 router.all("/", (req, res, next) => {
-    freeShadowsocks().then(data => {
-        // console.log(r.data);
-        let ss = [];
-        data.forEach(v => {
-            let name = Base64.encode(`iShadowsocks free`);
-            let group = Base64.encode("iShadowsocks");
 
-            let address = `${v.address}:${v.port}:origin:${v.method}:plain:${v.password}/?remarks=${name}&group=${group}`;
-            let s = "ssr://" + Base64.encode(address).replace("=", "");
-            console.log(`ssr://${address}`);
-            ss.push(s);
-        });
-        res.status(200).send(Base64.encode(ss.join("\r\n")));
-    }).catch(err => {
-        res.status(404).send(err.message);
+
+ 
+    gitPullOrClone('git@github.com:feross/standard.git', path.join(process.cwd(), "sub"), (err) => {
+        if (err) return res.status(404).send(err);
+        let ss = fs.readFileSync(path.join(process.cwd(), "sub", "ssshare.com"), "utf8");
+        res.status(200).send(ss);
     });
+
+    // axios.get(`https://free-ss.site/ss.json?_=${new Date().getTime()}`).then(r => {
+    //     // console.log(r.data);
+    //     // let ss = [];
+    //     // r.data.data.forEach(v => {
+    //     //     let name = Base64.encode(`${v[6]} - iShadowsocks`);
+    //     //     let group = Base64.encode("iShadowsocks");
+    //     //     let ip = v[1],
+    //     //         port = v[2],
+    //     //         code = v[3],
+    //     //         pass = v[4];
+    //     //     let address = `${ip}:${port}:origin:${code}:plain:${pass}/?remarks=${name}&group=${group}`;
+    //     //     let s = "ssr://" + Base64.encode(address).replace("=", "");
+    //     //     console.log(`ssr://${address}`);
+    //     //     ss.push(s);
+    //     // });
+    //     // res.status(200).send(Base64.encode(ss.join("\r\n")));
+    //     res.status(200).send(r.data);
+    // }).catch(err => {
+    //     res.status(404).send(err.message);
+    // });
 });
